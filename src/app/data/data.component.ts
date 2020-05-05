1e38c3fa3cd32920b003b2b60ed3f70cbed37f66
ThD <thibault.delorme@protonmail.com>
2020-05-05 13:01:12 +0200

    display spinner when data is unavailable in data component

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -8,14 +8,47 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DataComponent implements OnInit {
 
-  private country: string;
+  private _country: string;
+  private _confirmed: number;
+  private _recovered: number;
+  private _critical: number;
+  private _deaths: number;
+
+  private confirmed(): string {
+    if (this._confirmed == -1)
+      return "...";
+    return this._confirmed.toString();
+  }
+
+  private recovered(): string {
+    if (this._recovered == -1)
+      return "...";
+    return this._recovered.toString();
+  }
+
+  private critical(): string {
+    if (this._critical == -1)
+      return "...";
+    return this._critical.toString();
+  }
+
+  private deaths(): string {
+    if (this._deaths == -1)
+      return "...";
+    return this._deaths.toString();
+  }
 
   constructor(private route: ActivatedRoute) {
-    this.country = this.route.snapshot.paramMap.get('country');
-    if (this.country !== null)
-      console.log("Display data for country " + this.country);
+    this._country = this.route.snapshot.paramMap.get('country');
+    if (this._country !== null)
+      console.log("Display data for country " + this._country);
     else
       console.log("Display data for the entire world");
+
+    this._confirmed = -1;
+    this._recovered = -1;
+    this._critical = -1;
+    this._deaths = -1;
 
   }
 
